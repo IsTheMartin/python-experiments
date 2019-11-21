b2c2dad66d1483916a9e7b7d794513a6bcce24dc
Martin Morales <isthemartin@gmail.com>
2019-11-20 22:33:29 -0600

    Added threads and more info
    Added a thread for download process.
    Added a combo box listener, now you can see filesize for each resolution.
    Added a progress percent when file is downloading.

--- VideoDownloader/main.py
+++ VideoDownloader/main.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 from tkinter import messagebox
 from decimal import Decimal
 import re
+import threading
+from hurry.filesize import size
 
 url = ""
 downloadPath = ""
@@ -12,6 +14,7 @@ downloadPath = ""
 class Application(tk.Frame):
 
     video = None
+    MaxfileSize = 0
 
     def __init__(self, main_window):
         super().__init__(main_window)
@@ -20,7 +23,7 @@ class Application(tk.Frame):
         self.lblUrl = ttk.Label(main_window, text="Paste url:")
         self.lblUrl.place(x=10, y=10)
         
-        self.txtUrl = ttk.Entry(main_window)
+        self.txtUrl = ttk.Entry(main_window, width=70)
         self.txtUrl.place(x=100, y=10)
 
         self.btnAnalyzer = ttk.Button(main_window, text="Analize", command=self.analizeUrl)
@@ -32,17 +35,22 @@ class Application(tk.Frame):
         self.lblDuration = ttk.Label(main_window, text="Duration: ")
         self.lblDuration.place(x=10, y=100)
 
-        self.cmbStreams = ttk.Combobox(main_window, width=70)
+        self.cmbStreams = ttk.Combobox(main_window, width=70, state='readonly')
         self.cmbStreams.place(x=10, y=120)
+        self.cmbStreams.bind("<<ComboboxSelected>>", self.showFileSize)
+
+        self.lblFileSize = ttk.Label(main_window, text="Size: ")
+        self.lblFileSize.place(x=10, y=150)
 
         self.btnDownload = ttk.Button(main_window, text="Download", command=self.downloadMedia)
-        self.btnDownload.place(x=10, y=150)
+        self.btnDownload.place(x=10, y=180)
+
+        self.lblLoadingPercent = ttk.Label(main_window, text="Idle")
+        self.lblLoadingPercent.place(x=10, y=200)
 
         main_window.configure(width=500, height=300)
         self.place(width=300, height=200)
 
-    
-        print(str(100))
 
     def analizeUrl(self):
         if self.txtUrl.get() != "":
@@ -63,22 +71,30 @@ class Application(tk.Frame):
     def populateStreams(self, streams):
         self.cmbStreams["values"] = streams
 
-    def show(self):
-        pass
+    def showFileSize(self, event):
+        if self.cmbStreams.current() > -1 and self.video is not None:
+            itag = re.search('itag="(.*)" mime', self.cmbStreams.get())
+            videotype = self.video.streams.get_by_itag(itag.group(1))
+            self.MaxfileSize = videotype.filesize
+            self.lblFileSize.config(text="File size: " + str(size(self.MaxfileSize)))
 
     def downloadMedia(self):
         if self.cmbStreams.current() > -1 and self.video is not None:
             try:
                 itag = re.search('itag="(.*)" mime', self.cmbStreams.get())
-                self.video.register_on_progress_callback(self.showProgressBar)
-                self.video.streams.get_by_itag(itag.group(1)).download()
+                threading.Thread(target=self.video.register_on_progress_callback(self.showProgressBar))
+                threading.Thread(target=self.launchThread, args=(itag,)).start()
             except NameError as err:
                 print(messagebox.showwarning(message=str(err)))
-        else:
-            pass
+    
+    def launchThread(self, itag):
+
+        videotype = self.video.streams.get_by_itag(itag.group(1))
+        self.MaxfileSize = videotype.filesize
+        videotype.download()
                 
     def showProgressBar(self, stream=None, chunk=None, file_handle=None, bytes_remaining=None):
-        print("")
+        self.lblLoadingPercent.config(text=str(int(100 - (100*(bytes_remaining/self.MaxfileSize)))))
 
 main_window = tk.Tk()
 app = Application(main_window)
